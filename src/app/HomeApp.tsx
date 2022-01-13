aa6523097206dde3bf54269b3e90c090ea3bc9b6
yasmin.ghassani <yasmin.ghassani@wartek.belajar.id>
2022-01-13 13:49:45 +0700

    [SDSADM-651] add clsx

--- src/app/HomeApp.tsx
+++ src/app/HomeApp.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from "react";
+import clsx from "clsx";
 
 const HomeApp: FC = () => {
-  return <div className="text-pink-600 text-center">Home App</div>;
+  return <div className={clsx("text-pink-600", "text-center")}>Home App</div>;
 };
 
 export default HomeApp;
