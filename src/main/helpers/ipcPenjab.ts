41ed090b5160e8f5d486a9093510d3ebbb49d744
Fuad Kleb <fuadkleb@yahoo.com>
2022-03-11 01:37:51 +0000

    Feature/sdsadm 908

---
+++ src/main/helpers/ipcPenjab.ts
@@ -0,0 +1,50 @@
+import { ipcMain } from 'electron'
+import { SekolahPenjab } from 'main/repositories/SekolahPenjab'
+import {
+  addSekolahPenjab,
+  findSekolahPenjabId,
+  updateSekolahPenjab,
+} from 'main/services/SekolahPenjab'
+
+module.exports = {
+  addPenjab: ipcMain.on('penjab:addPenjab', async (e, data) => {
+    const getYear = new Date().getFullYear().toString()
+    const dataPenjab = new SekolahPenjab()
+    dataPenjab.sekolahId = data.sekolah_id
+    dataPenjab.tanggalMulai = getYear + '-01-01'
+    dataPenjab.tanggalSelesai = getYear + '12-31'
+    dataPenjab.ks = data.kepsek
+    dataPenjab.nipKs = data.nip_kepsek
+    dataPenjab.emailKs = data.email_kepsek
+    dataPenjab.telpKs = data.telepon_kepsek
+    dataPenjab.bendahara = data.bendahara
+    dataPenjab.nipBendahara = data.nip_bendahara
+    dataPenjab.emailBendahara = data.email_bendahara
+    dataPenjab.telpBendahara = data.telepon_bendahara
+    dataPenjab.komite = data.komite
+    dataPenjab.nipKomite = data.nip_komite
+    await addSekolahPenjab(dataPenjab)
+    const idPenjab = await findSekolahPenjabId(dataPenjab)
+    e.returnValue = idPenjab
+  }),
+
+  setPenjab: ipcMain.on('penjab:updatePenjab', async (e, data) => {
+    const dataPenjab = new SekolahPenjab()
+    dataPenjab.ks = data.kepsek
+    dataPenjab.nipKs = data.nip_kepsek
+    dataPenjab.bendahara = data.bendahara
+    dataPenjab.nipBendahara = data.nip_bendahara
+    dataPenjab.komite = data.komite
+    dataPenjab.nipKomite = data.nip_komite
+
+    const idPenjab = await findSekolahPenjabId(dataPenjab)
+    dataPenjab.idPenjab = idPenjab
+
+    try {
+      await updateSekolahPenjab(dataPenjab)
+      e.returnValue = true
+    } catch {
+      e.returnValue = false
+    }
+  }),
+}
--- src/main/services/SekolahPenjab.ts
+++ src/main/services/SekolahPenjab.ts
@@ -1,27 +1,48 @@
-import { getRepository, InsertResult } from 'typeorm'
+import {
+  getConnection,
+  getRepository,
+  InsertResult,
+  UpdateResult,
+} from 'typeorm'
 import { SekolahPenjab } from '../repositories/SekolahPenjab'
 
 export const addSekolahPenjab = async (
   sekolahPenjab: SekolahPenjab
 ): Promise<InsertResult> => {
-  return await getRepository(SekolahPenjab).insert({
-    idPenjab: sekolahPenjab.idPenjab,
-    sekolahId: sekolahPenjab.sekolahId,
-    tanggalMulai: sekolahPenjab.tanggalMulai,
-    tanggalSelesai: sekolahPenjab.tanggalSelesai,
-    ks: sekolahPenjab.ks,
-    nipKs: sekolahPenjab.nipKs,
-    emailKs: sekolahPenjab.emailKs,
-    telpKs: sekolahPenjab.telpKs,
-    bendahara: sekolahPenjab.bendahara,
-    nipBendahara: sekolahPenjab.nipBendahara,
-    emailBendahara: sekolahPenjab.emailBendahara,
-    telpBendahara: sekolahPenjab.telpBendahara,
-    komite: sekolahPenjab.komite,
-    nipKomite: sekolahPenjab.nipKomite,
-    softDelete: sekolahPenjab.softDelete,
-    createDate: sekolahPenjab.createDate,
-    lastUpdate: sekolahPenjab.lastUpdate,
-    updaterId: sekolahPenjab.updaterId,
-  })
+  return await getRepository(SekolahPenjab).upsert(sekolahPenjab, ['idPenjab'])
+}
+
+export const findSekolahPenjabId = async (
+  sekolahPenjab: SekolahPenjab
+): Promise<string> => {
+  return (
+    (
+      await getRepository(SekolahPenjab).findOne({
+        ks: sekolahPenjab.ks,
+        nipKs: sekolahPenjab.nipKs,
+        bendahara: sekolahPenjab.bendahara,
+        nipBendahara: sekolahPenjab.nipBendahara,
+        komite: sekolahPenjab.komite,
+        nipKomite: sekolahPenjab.nipKomite,
+      })
+    )?.idPenjab ?? ''
+  )
+}
+
+export const updateSekolahPenjab = async (
+  sekolahPenjab: SekolahPenjab
+): Promise<UpdateResult> => {
+  return await getConnection()
+    .createQueryBuilder()
+    .update(SekolahPenjab)
+    .set({
+      ks: sekolahPenjab.ks,
+      nipKs: sekolahPenjab.nipKs,
+      bendahara: sekolahPenjab.bendahara,
+      nipBendahara: sekolahPenjab.nipBendahara,
+      komite: sekolahPenjab.komite,
+      nipKomite: sekolahPenjab.nipKomite,
+    })
+    .where({ idPenjab: sekolahPenjab.idPenjab })
+    .execute()
 }
