cc7701d4faf3698bdd8c8a0a60ad9e0e1d7665f4
yasmin.ghassani <yasmin.ghassani@wartek.belajar.id>
2022-04-04 11:23:34 +0700

    tes

--- installer-win/installer-win.ts
+++ installer-win/installer-win.ts
@@ -3,11 +3,9 @@ import { execFile } from 'child_process'
 import { promisify } from 'util'
 
 export const InstallerWin = (): void => {
-  // if (process.platform == 'win32') {
   const paramStr = path.join(__dirname, 'arkas.4.00.iss')
   const execPromisify = promisify(execFile)
   execPromisify(path.join(__dirname, 'inno', 'ISCC.exe'), [paramStr])
-  // }
 }
 
 InstallerWin()
