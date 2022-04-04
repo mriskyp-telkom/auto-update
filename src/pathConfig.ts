59919583988fdb275e83891cbf0d923ec21c4611
mriskyp <1nfhikaru@gmail.com>
2022-04-03 19:42:27 -0700

    set path config and fix issues

--- src/pathConfig.ts
+++ src/pathConfig.ts
@@ -43,7 +43,6 @@ export async function getAppData(): Promise<string> {
   }
   return envPath
 }
-xw
 
 export async function getManualAppData(): Promise<string> {
   // change as your config local exists
