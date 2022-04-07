9e1b8ac15bb2f7326d62ebaa3d076678d9a128b5
Risky Putro <risky.negoro@wartek.belajar.id>
2022-04-07 02:24:19 +0000

    set unit test user role

--- tests/main/db/seeds/006_seed-user-role.sql
+++ tests/main/db/seeds/006_seed-user-role.sql
@@ -1,12 +1,31 @@
-INSERT INTO user_role (userrole_id, role_id, instansi_pengguna_id, soft_delete, create_date, last_update, updater_id) VALUES 
- ('NkFEQIt0a0aGl5V0uE89sw', 'kG0LFX0OXEuso0DtvGmniw', 'pLLd-OgaHEytKXrvx5pK4g', 0, '2017-02-16 14:49:15.000', '2017-02-16 14:49:15.000', 'XN9a0PUuEeC4Hgl8dIQlSw'),
- ('lcOrsXpark6cwe_kYy4CgA', 'kG0LFX0OXEuso0DtvGmniw', 'tvIvvU8HxkO85WEiEKw2gg', 0, '2017-02-24 14:04:12.000', '2017-02-24 14:04:12.000', 'XOCUQPUuEeCIQENDPIlsag'),
- ('2idmFXmV9kuKH1qUVOrIdg', 'kG0LFX0OXEuso0DtvGmniw', 'mR0QnRogkk2u4j9B048RTg', 0, '2020-03-09 15:35:30.000', '2020-03-09 15:35:30.000', 'EREREQAA__-Zmf_______w');
+INSERT INTO user_role (userrole_id, role_id, instansi_pengguna_id, soft_delete,
+    create_date, last_update, updater_id) VALUES 
+ ('NkFEQIt0a0aGl5V0uE89sw', 'kG0LFX0OXEuso0DtvGmniw', 'pLLd-OgaHEytKXrvx5pK4g',
+    0, '2017-02-16 14:49:15.000', '2017-02-16 14:49:15.000', 'XN9a0PUuEeC4Hgl8dIQlSw'),
+ ('lcOrsXpark6cwe_kYy4CgA', 'kG0LFX0OXEuso0DtvGmniw', 'tvIvvU8HxkO85WEiEKw2gg',
+    0, '2017-02-24 14:04:12.000', '2017-02-24 14:04:12.000', 'XOCUQPUuEeCIQENDPIlsag'),
+ ('2idmFXmV9kuKH1qUVOrIdg', 'kG0LFX0OXEuso0DtvGmniw', 'mR0QnRogkk2u4j9B048RTg',
+    0, '2020-03-09 15:35:30.000', '2020-03-09 15:35:30.000', 'EREREQAA__-Zmf_______w');
 
-INSERT INTO instansi_pengguna (instansi_pengguna_id, pengguna_id, instansi_id, jabatan_id, sk, tanggal_sk, soft_delete, create_date, last_update, updater_id) VALUES 
-('fqrYww_0p0W7mF0GCksfkw', 'A-miBPPeP0m2UQaNDV6DHA', 'RF8ExGeBk0ewknCtEPAkWQ', 1, '-', '2017-02-14 11:51:55.000', 0, '2017-02-14 11:51:30.000', '2017-02-14 11:51:34.000', 'BAAAAAQABAAABAAAAAAABA'),
-('mR0QnRogkk2u4j9B048RTg', 'pUe9yWUZHkmYezDiz7DTDA', 'XN60oPUuEeC-vv2_lhMTXQ', 10, '-', NULL, 0, '2020-03-09 15:35:30.000', '2020-03-09 15:35:30.000', 'EREREQAA__-Zmf_______w');
+INSERT INTO instansi_pengguna (instansi_pengguna_id, pengguna_id, instansi_id,
+    jabatan_id, sk, tanggal_sk,
+    soft_delete, create_date, last_update,
+    updater_id) VALUES 
+('fqrYww_0p0W7mF0GCksfkw', 'A-miBPPeP0m2UQaNDV6DHA', 'RF8ExGeBk0ewknCtEPAkWQ',
+    1, '-', '2017-02-14 11:51:55.000',
+    0, '2017-02-14 11:51:30.000', '2017-02-14 11:51:34.000',
+    'BAAAAAQABAAABAAAAAAABA'),
+('mR0QnRogkk2u4j9B048RTg', 'pUe9yWUZHkmYezDiz7DTDA', 'XN60oPUuEeC-vv2_lhMTXQ',
+    10, '-', NULL,
+    0, '2020-03-09 15:35:30.000', '2020-03-09 15:35:30.000',
+    'EREREQAA__-Zmf_______w');
 
-INSERT INTO pengguna (pengguna_id, nama, tempat_lahir, tanggal_lahir, jenis_kelamin, alamat, kode_pos, telepon, show_telepon, nip, kode_wilayah, email, password, forgot_pass1, forgot_pass2, status_approval, soft_delete, create_date, last_update, updater_id) VALUES 
+INSERT INTO pengguna (pengguna_id, nama, tempat_lahir,
+    tanggal_lahir, jenis_kelamin, alamat,
+    kode_pos, telepon, show_telepon,
+    nip, kode_wilayah, email,
+    password, forgot_pass1, forgot_pass2,
+    status_approval, soft_delete, create_date,
+    last_update, updater_id) VALUES 
 ('A-miBPPeP0m2UQaNDV6DHA', 'Administrator', '-', '2017-02-14', 'L', '-', 0, '0', 0, '-', '0', 'admin@dikpora.gk', '21232f297a57a5a743894a0e4a801fc3', NULL, NULL, 1, 0, '2017-02-14 11:46:48.000', '2017-02-14 11:46:45.000', 'BAAAAAQABAAABAAAAAAABA'),
 ('pUe9yWUZHkmYezDiz7DTDA', 'WASIDI', '-', '1901-01-01', 'L', '-', 0, '085228787703', 1, '0', '040300', 'wasidi117@gmail.com', '25D55AD283AA400AF464C76D713C07AD', NULL, NULL, 1, 0, '2020-03-09 15:35:30.000', '2020-03-09 15:35:30.000', 'EREREQAA__-Zmf_______w');
--- tests/main/repositories/User.test.ts
+++ tests/main/repositories/User.test.ts
@@ -5,8 +5,19 @@ import { Instansi } from 'main/models/Instansi'
 import { MstSekolah } from 'main/models/MstSekolah'
 import { MstWilayah } from 'main/models/MstWilayah'
 import { Pengguna } from 'main/models/Pengguna'
+import { Token } from 'main/models/Token'
 import { UserRole } from 'main/models/UserRole'
-import { CheckUser, CheckUserPass } from 'main/repositories/User'
+import CommonUtils from 'main/utils/CommonUtils'
+
+import {
+  CheckUser,
+  CheckUserPass,
+  CheckLogin,
+  GetUserRole,
+  GetPenggunaByEmail,
+  GetPenggunaByToken,
+  AddPengguna,
+} from 'main/repositories/User'
 import { createConnection, getConnection } from 'typeorm'
 import { cfg, Migrate } from '../migration'
 
@@ -27,6 +38,7 @@ beforeEach(async () => {
       MstSekolah,
       MstWilayah,
       Pengguna,
+      Token,
       UserRole,
     ],
     synchronize: false,
@@ -44,13 +56,70 @@ afterEach(async () => {
 
 test('CheckUser', async () => {
   const userIsExist = await CheckUser('wasidi117@gmail.com')
+
   expect(userIsExist).toBe(true)
 })
 
 test('CheckUserPass', async () => {
-  const passwordIsCorret = await CheckUserPass(
+  const passwordIsCorrect = await CheckUserPass(
     'wasidi117@gmail.com',
     '12345678'
   )
-  expect(passwordIsCorret).toBe(true)
+
+  expect(passwordIsCorrect).toBe(true)
+})
+
+test('CheckLogin', async () => {
+  const checkLogin = await CheckLogin()
+
+  expect(checkLogin).toBe(2)
+})
+
+test('GetUserRole', async () => {
+  const username = 'wasidi117@gmail.com'
+  const checkUserRole = await GetUserRole(username)
+
+  expect(checkUserRole.instansiPenggunaId).toBe('mR0QnRogkk2u4j9B048RTg')
+  expect(checkUserRole.roleId).toBe('kG0LFX0OXEuso0DtvGmniw')
+  expect(checkUserRole.userroleId).toBe('2idmFXmV9kuKH1qUVOrIdg')
+})
+
+test('GetPenggunaByEmail', async () => {
+  const username = 'wasidi117@gmail.com'
+  const data = await GetPenggunaByEmail(username)
+
+  expect(data.penggunaId).toBe('pUe9yWUZHkmYezDiz7DTDA')
+  expect(data.nip).toBe('0')
+  expect(data.nama).toBe('WASIDI')
+  expect(data.email).toBe('wasidi117@gmail.com')
+  expect(data.updaterId).toBe('EREREQAA__-Zmf_______w')
+})
+
+test('GetPenggunaByToken', async () => {
+  const tokenId = CommonUtils.encodeUUID(CommonUtils.uuid())
+  const data = await GetPenggunaByToken(tokenId)
+
+  expect(data).toBe(undefined)
+})
+
+test('AddPengguna', async () => {
+  const newDate = new Date()
+  const pengguna = new Pengguna()
+  pengguna.penggunaId = 'pUe9yWUZHkmYezDiz7DTDB'
+  pengguna.nama = 'wasidi'
+  pengguna.jenisKelamin = 'L'
+  pengguna.tanggalLahir = new Date('2017-02-14')
+  pengguna.tempatLahir = 'abcd'
+  pengguna.email = 'wasidi117@gmail.com'
+  pengguna.password = 'abcd'
+  pengguna.createDate = newDate
+  pengguna.lastUpdate = newDate
+  pengguna.updaterId = 'EREREQAA__-Zmf_______w'
+  pengguna.softDelete = 0
+  pengguna.statusApproval = 0
+  const insertResult = await AddPengguna(pengguna)
+
+  // check generated maps
+  const data = insertResult.generatedMaps
+  expect(data.length).toBeGreaterThan(0)
 })
