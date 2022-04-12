e377ea144599be29dbe1a827c225e55d853e7206
Hidayatullah Agung Prasetyo <hidayatullahap@wartek.belajar.id>
2022-04-12 09:28:36 +0000

    SDSADM-1321

--- tests/main/services/RapbsService.test.ts
+++ tests/main/services/RapbsService.test.ts
@@ -2,12 +2,15 @@ import { AppConfig } from 'main/models/AppConfig'
 import { Rapbs } from 'main/models/Rapbs'
 import { RapbsPeriode } from 'main/models/RapbsPeriode'
 import { RapbsPtk } from 'main/models/RapbsPtk'
-import { AddDetailKegiatan } from 'main/services/KertasKerjaService'
+import {
+  AddDetailKegiatan,
+  UpdateDetailKegiatan,
+} from 'main/services/KertasKerjaService'
 import {
   DetailKegiatan,
   Periode,
   Ptk,
-  ResultDetailKegiatan,
+  ResultAddDetailKegiatan,
 } from 'main/types/Rapbs'
 import { createConnection, getConnection, getRepository } from 'typeorm'
 import { cfg, Migrate } from '../migration'
@@ -50,7 +53,7 @@ test('AddDetailKegiatanEmptyPtkPeriode', async () => {
   const res = await AddDetailKegiatan(data)
   expect(res.isOk()).toBe(true)
 
-  let ids = <ResultDetailKegiatan>{}
+  let ids = <ResultAddDetailKegiatan>{}
   res.map((responseData) => {
     ids = responseData
   })
@@ -70,7 +73,7 @@ test('AddDetailKegiatanEmptyPtkPeriode', async () => {
   expect(rapbs.jumlah).toBe(data.jumlah)
 })
 
-test('AddDetailKegiatanSuccess', async () => {
+test('AddandUpdateDetailKegiatanSuccess', async () => {
   const data = <DetailKegiatan>{
     idAnggaran: 'apQwiAb-9EWxv74iwMY6aQ',
     idRefKode: 'nC6K24ZodkO3wkcvW3wvYg',
@@ -96,10 +99,10 @@ test('AddDetailKegiatanSuccess', async () => {
         jumlah: 10,
       },
       {
-        idPeriode: 84,
-        volume: 2000,
-        satuan: 'Rim',
-        hargaSatuan: 200,
+        idPeriode: 85,
+        volume: 2500,
+        satuan: 'Buah',
+        hargaSatuan: 2500,
         jumlah: 10,
       },
     ],
@@ -108,7 +111,7 @@ test('AddDetailKegiatanSuccess', async () => {
   const res = await AddDetailKegiatan(data)
   expect(res.isOk()).toBe(true)
 
-  let ids = <ResultDetailKegiatan>{}
+  let ids = <ResultAddDetailKegiatan>{}
   res.map((responseData) => {
     ids = responseData
   })
@@ -128,15 +131,23 @@ test('AddDetailKegiatanSuccess', async () => {
   expect(rapbs.jumlah).toBe(data.jumlah)
   expect(rapbs.urutan).toBe('009')
 
-  const periode = await getRepository(RapbsPeriode).findOne({
-    idRapbsPeriode: ids.idRapbsPeriode[0],
+  const periode = await getRepository(RapbsPeriode).find({
+    idRapbs: ids.idRapbs,
+    softDelete: 0,
   })
 
-  expect(periode.idPeriode).toBe(data.periode[0].idPeriode)
-  expect(periode.satuan).toBe(data.periode[0].satuan)
-  expect(periode.volume).toBe(data.periode[0].volume)
-  expect(periode.hargaSatuan).toBe(data.periode[0].hargaSatuan)
-  expect(periode.jumlah).toBe(data.periode[0].jumlah)
+  expect(periode.length).toBe(2)
+  expect(periode[0].idPeriode).toBe(data.periode[0].idPeriode)
+  expect(periode[0].satuan).toBe(data.periode[0].satuan)
+  expect(periode[0].volume).toBe(data.periode[0].volume)
+  expect(periode[0].hargaSatuan).toBe(data.periode[0].hargaSatuan)
+  expect(periode[0].jumlah).toBe(data.periode[0].jumlah)
+
+  expect(periode[1].idPeriode).toBe(data.periode[1].idPeriode)
+  expect(periode[1].satuan).toBe(data.periode[1].satuan)
+  expect(periode[1].volume).toBe(data.periode[1].volume)
+  expect(periode[1].hargaSatuan).toBe(data.periode[1].hargaSatuan)
+  expect(periode[1].jumlah).toBe(data.periode[1].jumlah)
 
   const ptk = await getRepository(RapbsPtk).findOne({
     idRapbs: ids.idRapbs,
@@ -144,4 +155,88 @@ test('AddDetailKegiatanSuccess', async () => {
   })
 
   expect(ptk.nama).toBe(data.ptk.nama)
+
+  const currentRapbsId = ids.idRapbs
+  const dataUpdate = <DetailKegiatan>{
+    idRapbs: currentRapbsId,
+    idAnggaran: 'apQwiAb-9EWxv74iwMY6aQ',
+    idRefKode: 'nC6K24ZodkO3wkcvW3wvYg',
+    idRefTahunAnggaran: 2021,
+    kodeRekening: '5.1.02.01.01.0024',
+    idBarang: '01.200',
+    uraian: 'Pensil 2B Joyko P-88 Hijau (12 Pcs)',
+    satuan: 'Paket',
+    urutan: '009',
+    volume: 5,
+    hargaSatuan: 10000,
+    jumlah: 50000,
+    createDate: rapbs.createDate,
+    ptk: null,
+    periode: <Periode[]>[
+      {
+        idPeriode: 84,
+        volume: 5,
+        satuan: 'Buah',
+        hargaSatuan: 10000,
+        jumlah: 50000,
+      },
+      {
+        idPeriode: 86,
+        volume: 1,
+        satuan: 'Buah',
+        hargaSatuan: 15000,
+        jumlah: 15000,
+      },
+      {
+        idPeriode: 87,
+        volume: 7,
+        satuan: 'Buah',
+        hargaSatuan: 300,
+        jumlah: 21000,
+      },
+    ],
+  }
+
+  const resUpdate = await UpdateDetailKegiatan(dataUpdate)
+  expect(resUpdate.isOk()).toBe(true)
+
+  const rapbsUpdate = await getRepository(Rapbs).findOne({
+    idRapbs: currentRapbsId,
+    softDelete: 0,
+  })
+
+  expect(rapbsUpdate.idAnggaran).toBe(dataUpdate.idAnggaran)
+  expect(rapbsUpdate.idRefKode).toBe(dataUpdate.idRefKode)
+  expect(rapbsUpdate.kodeRekening).toBe(dataUpdate.kodeRekening)
+  expect(rapbsUpdate.idBarang).toBe(dataUpdate.idBarang)
+  expect(rapbsUpdate.uraian).toBe(dataUpdate.uraian)
+  expect(rapbsUpdate.satuan).toBe(dataUpdate.satuan)
+  expect(rapbsUpdate.volume).toBe(dataUpdate.volume)
+  expect(rapbsUpdate.hargaSatuan).toBe(dataUpdate.hargaSatuan)
+  expect(rapbsUpdate.jumlah).toBe(dataUpdate.jumlah)
+  expect(rapbsUpdate.urutan).toBe(dataUpdate.urutan)
+
+  const periodeUpdate = await getRepository(RapbsPeriode).find({
+    idRapbs: currentRapbsId,
+    softDelete: 0,
+  })
+
+  expect(periodeUpdate.length).toBe(3)
+  expect(periodeUpdate[0].idPeriode).toBe(dataUpdate.periode[0].idPeriode)
+  expect(periodeUpdate[0].satuan).toBe(dataUpdate.periode[0].satuan)
+  expect(periodeUpdate[0].volume).toBe(dataUpdate.periode[0].volume)
+  expect(periodeUpdate[0].hargaSatuan).toBe(dataUpdate.periode[0].hargaSatuan)
+  expect(periodeUpdate[0].jumlah).toBe(dataUpdate.periode[0].jumlah)
+
+  expect(periodeUpdate[1].idPeriode).toBe(dataUpdate.periode[1].idPeriode)
+  expect(periodeUpdate[1].satuan).toBe(dataUpdate.periode[1].satuan)
+  expect(periodeUpdate[1].volume).toBe(dataUpdate.periode[1].volume)
+  expect(periodeUpdate[1].hargaSatuan).toBe(dataUpdate.periode[1].hargaSatuan)
+  expect(periodeUpdate[1].jumlah).toBe(dataUpdate.periode[1].jumlah)
+
+  expect(periodeUpdate[2].idPeriode).toBe(dataUpdate.periode[2].idPeriode)
+  expect(periodeUpdate[2].satuan).toBe(dataUpdate.periode[2].satuan)
+  expect(periodeUpdate[2].volume).toBe(dataUpdate.periode[2].volume)
+  expect(periodeUpdate[2].hargaSatuan).toBe(dataUpdate.periode[2].hargaSatuan)
+  expect(periodeUpdate[2].jumlah).toBe(dataUpdate.periode[2].jumlah)
 })
