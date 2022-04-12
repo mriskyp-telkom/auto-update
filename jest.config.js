8d6c78ce347928ad021f43d6baa8eece8ed2aaa2
mriskyp <1nfhikaru@gmail.com>
2022-04-12 10:01:21 -0700

    add yarn test

--- jest.config.js
+++ jest.config.js
@@ -25,7 +25,7 @@ module.exports = {
       },
     },
   ],
-  collectCoverageFrom: ['src/**/*.js', '!**/node_modules/**'],
+  collectCoverageFrom: ['src/**/*.js', 'src/**/*.ts', '!**/node_modules/**'],
   coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
-  testMatch: ['**/*.test.js'],
+  testMatch: ['**/*.test.js', '**/*.test.ts'],
 }
